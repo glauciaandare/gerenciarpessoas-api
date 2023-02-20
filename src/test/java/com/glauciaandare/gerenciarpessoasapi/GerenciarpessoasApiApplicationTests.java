69885f82ec87e8db3fd020cc6b91954b707b5ad9
Gláucia Cecília Pivoto Andare <gandare@gmail.com>
2023-02-19 23:29:07 -0300

    Configurando aplicação

---
+++ src/test/java/com/glauciaandare/gerenciarpessoasapi/GerenciarpessoasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glauciaandare.gerenciarpessoasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarpessoasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
